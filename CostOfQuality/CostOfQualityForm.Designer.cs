673b3bd3bac17ac20903efd5244db90cd1791f9a
unknown <dleiva@ATI-PC063.ati.local>
2016-03-12 12:16:18 -0800

    Added hours column. First query updated

--- CostOfQuality/CostOfQualityForm.Designer.cs
+++ CostOfQuality/CostOfQualityForm.Designer.cs
@@ -29,12 +29,17 @@
         private void InitializeComponent()
         {
             this.label11 = new System.Windows.Forms.Label();
-            this.Calculate = new System.Windows.Forms.Button();
+            this.calculateButton = new System.Windows.Forms.Button();
             this.startDateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.endDateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.DetailsDataGridView = new System.Windows.Forms.DataGridView();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Hours = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.DetailsDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,19 +53,18 @@
             this.label11.TabIndex = 21;
             this.label11.Text = "Cost of Quality";
             // 
-            // Calculate
+            // calculateButton
             // 
-            this.Calculate.Location = new System.Drawing.Point(307, 241);
-            this.Calculate.Name = "Calculate";
-            this.Calculate.Size = new System.Drawing.Size(75, 23);
-            this.Calculate.TabIndex = 22;
-            this.Calculate.Text = "Calculate";
-            this.Calculate.UseVisualStyleBackColor = true;
-            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            this.calculateButton.Location = new System.Drawing.Point(397, 201);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 22;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
             // 
             // startDateTimePicker
             // 
-            this.startDateTimePicker.Location = new System.Drawing.Point(307, 122);
+            this.startDateTimePicker.Location = new System.Drawing.Point(397, 93);
             this.startDateTimePicker.Name = "startDateTimePicker";
             this.startDateTimePicker.Size = new System.Drawing.Size(200, 20);
             this.startDateTimePicker.TabIndex = 23;
@@ -68,7 +72,7 @@
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(307, 106);
+            this.label12.Location = new System.Drawing.Point(397, 77);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(55, 13);
             this.label12.TabIndex = 24;
@@ -77,7 +81,7 @@
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(307, 164);
+            this.label13.Location = new System.Drawing.Point(397, 135);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(52, 13);
             this.label13.TabIndex = 26;
@@ -85,7 +89,7 @@
             // 
             // endDateTimePicker
             // 
-            this.endDateTimePicker.Location = new System.Drawing.Point(307, 180);
+            this.endDateTimePicker.Location = new System.Drawing.Point(397, 151);
             this.endDateTimePicker.Name = "endDateTimePicker";
             this.endDateTimePicker.Size = new System.Drawing.Size(200, 20);
             this.endDateTimePicker.TabIndex = 25;
@@ -99,23 +103,69 @@
             this.DetailsDataGridView.AllowUserToResizeRows = false;
             this.DetailsDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.DetailsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            this.DetailsDataGridView.Location = new System.Drawing.Point(30, 81);
+            this.DetailsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Value,
+            this.Hours});
+            this.DetailsDataGridView.Location = new System.Drawing.Point(29, 75);
             this.DetailsDataGridView.Name = "DetailsDataGridView";
             this.DetailsDataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
-            this.DetailsDataGridView.Size = new System.Drawing.Size(232, 246);
+            this.DetailsDataGridView.Size = new System.Drawing.Size(335, 246);
             this.DetailsDataGridView.TabIndex = 27;
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Cost";
+            this.Value.Name = "Value";
+            this.Value.ReadOnly = true;
+            // 
+            // Hours
+            // 
+            this.Hours.HeaderText = "Hours";
+            this.Hours.Name = "Hours";
+            this.Hours.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 346);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 22);
+            this.label1.TabIndex = 28;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(417, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 20);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Note: Development Copy";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(552, 310);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Ver 2.1.1";
             // 
             // CostOfQualityForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(646, 470);
+            this.ClientSize = new System.Drawing.Size(624, 344);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.DetailsDataGridView);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.endDateTimePicker);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.startDateTimePicker);
-            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.calculateButton);
             this.Controls.Add(this.label11);
             this.Name = "CostOfQualityForm";
             this.Text = "Cost Of Quality";
@@ -129,12 +179,17 @@
         #endregion
 
         private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Button calculateButton;
         private System.Windows.Forms.DateTimePicker startDateTimePicker;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.DateTimePicker endDateTimePicker;
         private System.Windows.Forms.DataGridView DetailsDataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hours;
     }
 }
 
--- CostOfQuality/CostOfQualityForm.cs
+++ CostOfQuality/CostOfQualityForm.cs
@@ -8,19 +8,22 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.Odbc;
+
 namespace CostOfQuality
 {
     public partial class CostOfQualityForm : Form
     {
         enum DataType { DoubleT, StringT, DateT };
 
+        // global variables to connect to database
         private static string uni_DSN = "uniPointDB";
         private static string jobBoss_DSN = "uniPointDB";
         private readonly static string username = "jbread";
         private readonly static string password = "Cloudy2Day";
-        private readonly string[] fields = { "Scrap", "Labor", "Rework", "Recovery", "MRB Costs", "Lost Revenue", "Wasted Capacity", "Recovery Lot", "Delivery", "Reputation" };
         private readonly string uni_connectionString = "DSN=" + uni_DSN + ";UID=" + username + ";PWD=" + password;
         private readonly string jBoss_connectionString = "DSN=" + jobBoss_DSN + ";UID=" + username + ";PWD=" + password;
+        // all of the fields that the cost will be calculated for
+        private readonly string[] fields = { "Scrap Cost", "Quality Labor Cost", "Rework Cost", "MRB Cost", "Lost Revenue", "Wasted Capacity Cost", "Recovery Lot Cost", "Delivery Cost", "Reputation Cost" };        
 
         static List<ScrapItem> scrapList = new List<ScrapItem>();
 
@@ -29,122 +32,207 @@ namespace CostOfQuality
             InitializeComponent();
         }
 
-        private void Calculate_Click(object sender, EventArgs e)
-        {
-            FillInData();
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             // initialize gridview
-            DetailsDataGridView.Columns.Add("Value", "Value");
-
             for (int i = 0; i < fields.Length; i++)
             {
                 DetailsDataGridView.Rows.Add();
                 DetailsDataGridView.Rows[i].HeaderCell.Value = fields[i];
+                DetailsDataGridView.Rows[i].Cells[0].Style.Format = "C";
+                DetailsDataGridView.Rows[i].Cells[1].Style.Format = "#,##0 hrs";
             }
+            DetailsDataGridView.Columns["Value"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
 
             // initialize dateTimePicker
             startDateTimePicker.Value = startDateTimePicker.Value.AddYears(-1);
 
-            Calculate_Click(new Object(), new EventArgs());
+            FillInData(new object(), new EventArgs());
+
+            // add event handler for click
+            calculateButton.Click += this.FillInData;
         }
 
-        private void FillInData()
+        private void FillInData(object sender, EventArgs e)
         {
             string query;
             // calculate scrap cost
-            query = "SELECT 100*" + startDateTimePicker.Value.DayOfYear*endDateTimePicker.Value.DayOfYear;
-            ExecuteQuery(query, 0, DataType.DoubleT);
-
+            /*
+             * So far this only includes the internal operation costs
+             * Does not include: Outside ops, reworks scraps (tracing back to the original op)
+             */
+            query = "SELECT SUM(unionTotal.totalCost), SUM(unionTotal.totalHours) FROM\n" +
+                    "(\n" +
+                    "SELECT Sum((regJobs.Act_Run_Labor+regJobs.Act_Setup_Labor+regJobs.Act_Machine_Burden+regJobs.Act_Labor_Burden)/(regJobs.Act_Run_Qty+regJobs.Act_Scrap_Qty)*scrapCountT.Act_Scrap_Qty) AS totalCost,\n" +
+                    "\tSUM((regJobs.Act_Run_Labor_Hrs+regJobs.Act_Setup_Labor_Hrs)/(regJobs.Act_Run_Qty+regJobs.Act_Scrap_Qty)*scrapCountT.Act_Scrap_Qty) AS totalHours\n" +
+                    "FROM [PRODUCTION].[dbo].[Job_Operation] AS scrapCountT\n" +
+                    "LEFT OUTER JOIN PRODUCTION.dbo.Job_Operation as minRewOpT\n" +
+                    "ON minRewOpT.Job = scrapCountT.Job AND minRewOpT.Sequence <= scrapCountT.Sequence\n" +
+                    "INNER JOIN (\n" +
+                    "	SELECT Job, MIN(minT.Sequence) AS minPoint\n" +
+                    "	FROM PRODUCTION.dbo.Job_Operation as minT\n" +
+                    "	WHERE CHARINDEX('R', minT.Operation_Service) > 0 AND CHARINDEX('R', minT.Job) > 0\n" +
+                    "	GROUP BY Job\n" +
+                    ") AS minTable\n" +
+                    "ON scrapCountT.Job = minTable.Job\n" +
+                    "LEFT OUTER JOIN PRODUCTION.dbo.Job_Operation AS regJobs\n" +
+                    "ON regJobs.Job = SUBSTRING(scrapCountT.Job, 0, CHARINDEX('R', scrapCountT.Job)) AND regJobs.Run_Qty + regJobs.Act_Scrap_Qty > 0\n" +
+                    "WHERE scrapCountT.Act_Scrap_Qty > 0 AND scrapCountT.Last_Updated >= '" + startDateTimePicker.Value.ToShortDateString() + "' AND scrapCountT.Last_Updated <= '" + endDateTimePicker.Value.ToShortDateString() + "'\n" +
+                    "		AND ISNUMERIC((CASE WHEN(CHARINDEX('R', regJobs.Operation_Service) > 0) THEN SUBSTRING(regJobs.Operation_Service, 0, CHARINDEX('R', regJobs.Operation_Service)) ELSE regJobs.Operation_Service END)) = 1\n" +
+                    "		AND scrapCountT.Last_Updated <= '12/1/2015' AND minRewOpT.Sequence = minTable.minPoint \n" +
+                    "		AND CAST((CASE WHEN(CHARINDEX('R', regJobs.Operation_Service) > 0) THEN SUBSTRING(regJobs.Operation_Service, 0, CHARINDEX('R', regJobs.Operation_Service)) ELSE regJobs.Operation_Service END) AS int)\n" +
+                    "			<= CAST(SUBSTRING(minRewOpT.Operation_Service, 0, CHARINDEX('R', minRewOpT.Operation_Service)) AS int)\n" +
+                    "UNION ALL\n" +
+                    "SELECT Sum((sumCountT.Act_Run_Labor+sumCountT.Act_Setup_Labor+sumCountT.Act_Machine_Burden+sumCountT.Act_Labor_Burden)/(sumCountT.Act_Run_Qty+sumCountT.Act_Scrap_Qty)*scrapCountT.Act_Scrap_Qty) AS totalCost,\n" +
+                    "\tSum((sumCountT.Act_Run_Labor_Hrs+sumCountT.Act_Setup_Labor_Hrs)/(sumCountT.Act_Run_Qty+sumCountT.Act_Scrap_Qty)*scrapCountT.Act_Scrap_Qty) AS totalHours\n" +
+                    "FROM [PRODUCTION].[dbo].[Job_Operation] AS scrapCountT\n" +
+                    "LEFT OUTER JOIN [PRODUCTION].[dbo].[Job_Operation] AS sumCountT\n" +
+                    "ON scrapCountT.[Job] = sumCountT.[Job] AND sumCountT.Sequence <= scrapCountT.Sequence AND sumCountT.Act_Run_Qty+sumCountT.Act_Scrap_Qty > 0\n" +
+                    "WHERE scrapCountT.Act_Scrap_Qty > 0 AND scrapCountT.Last_Updated >= '" + startDateTimePicker.Value.ToShortDateString() + "' AND scrapCountT.Last_Updated <= '" + endDateTimePicker.Value.ToShortDateString() + "'\n" +
+                    "UNION ALL\n" +
+                    "SELECT ISNULL(SUM((mT.Act_Total_Cost/maxT.maxPoint)*jT.Act_Scrap_Qty), 0) AS totalCost,\n" +
+                    "\tISNULL(0, 0) AS totalHours\n" +
+                    "FROM PRODUCTION.dbo.Job_Operation jT\n" +
+                    "LEFT OUTER JOIN PRODUCTION.dbo.Material_Req AS mT\n" +
+                    "ON mT.Job = jT.Job\n" +
+                    "LEFT OUTER JOIN (\n" +
+                    "	SELECT Job, MAX(Act_Run_Qty) AS maxPoint\n" +
+                    "	FROM PRODUCTION.dbo.Job_Operation\n" +
+                    "	GROUP BY job\n" +
+                    ") AS maxT\n" +
+                    "ON maxT.Job = jT.Job\n" +
+                    "WHERE jT.Act_Scrap_Qty > 0 AND jT.Last_Updated >= '" + startDateTimePicker.Value.ToShortDateString() + "' AND jT.Last_Updated <= '" + endDateTimePicker.Value.ToShortDateString() + "' AND maxT.maxPoint > 0\n" +
+                    ") AS unionTotal;";
+            ExecuteQuery(query, 0, DataType.DoubleT, jBoss_connectionString);
+            /*
             // calculate labor cost
-            query = "SELECT 100*" + startDateTimePicker.Value.DayOfYear * endDateTimePicker.Value.DayOfYear;
-            ExecuteQuery(query, 1, DataType.DoubleT);
+            query = "SELECT SUM(CASE WHEN (NCR_Date >= '" + startDateTimePicker.Value.ToString() + "' AND NCR_Date <='" + endDateTimePicker.Value.ToString() + "') THEN NC_processing_cost ELSE 0 END)+\n" +
+                    "SUM(CASE WHEN (CPA_date >= '" + startDateTimePicker.Value.ToString() + "' AND CPA_date <='" + endDateTimePicker.Value.ToString() + "') THEN CPA_labor_cost ELSE 0 END)\n" +
+                    "FROM uniPoint_Live.dbo.PT_NC\n" +
+                    "FULL OUTER JOIN uniPoint_Live.dbo.PT_CPA\n" +
+                    "ON 1=2;";
+            ExecuteQuery(query, 1, DataType.DoubleT, uni_connectionString);
 
             // calculate rework cost
-            query = "SELECT 100*" + startDateTimePicker.Value.DayOfYear * endDateTimePicker.Value.DayOfYear;
-            ExecuteQuery(query, 2, DataType.DoubleT);
-
-            // calculate recovery cost
-            query = "SELECT 1000*" + startDateTimePicker.Value.DayOfYear * endDateTimePicker.Value.DayOfYear;
-            ExecuteQuery(query, 3, DataType.DoubleT);
+            query = "SELECT ISNULL(SUM(ISNULL(Act_Cost,0) + ISNULL(Addl_Cost_Act_Amt1,0) + ISNULL(Addl_Cost_Act_Amt2,0) + ISNULL(Job_Operation_Time.Act_Setup_Hrs*Setup_Labor_Rate,0) + ISNULL(Job_Operation_Time.Act_Run_Hrs*Run_Labor_Rate,0)), 0)\n" +
+                    "FROM Production.dbo.Job\n" +
+                    "INNER JOIN Production.dbo.Job_Operation\n" +
+                    "ON Job.Job = Job_Operation.Job\n" +
+                    "LEFT OUTER JOIN Production.dbo.PO_Detail\n" +
+                    "INNER JOIN Production.dbo.Source\n" +
+                    "ON PO_Detail.PO_Detail = Source.PO_Detail\n" +
+                    "ON Job_Operation.Job_Operation = Source.Job_Operation\n" +
+                    "LEFT OUTER JOIN Production.dbo.job_Operation_Time\n" +
+                    "ON Job_Operation.Job_Operation = Job_Operation_Time.Job_Operation\n" +
+                    "WHERE (Job_Operation.Operation_Service LIKE '%R%') AND (Job_Operation.Status = 'C') AND (NOT (Job.Part_Number LIKE 'NOT ISSUED')) AND\n" +
+                    "(Job.Order_Date >= '" + startDateTimePicker.Value.ToString() + "' AND Job.Order_Date <= '" + endDateTimePicker.Value.ToString() + "' AND Job.Status <> 'Template');";
+            ExecuteQuery(query, 2, DataType.DoubleT, jBoss_connectionString);
 
             // calculate MRB costs
-            query = "SELECT 100*" + startDateTimePicker.Value.DayOfYear * endDateTimePicker.Value.DayOfYear;
-            ExecuteQuery(query, 4, DataType.DoubleT);
+            query = "SELECT SUM(feesT.Fee)\n" +
+                    "FROM [uniPoint_Live].[dbo].[PT_NC] AS ncT\n" +
+                    "LEFT OUTER JOIN PRODUCTION.dbo.Material as matT\n" +
+                    "ON matT.Material = nct.Material\n" +
+                    "LEFT OUTER JOIN ATIDelivery.dbo.Customer_Fees as feesT\n" +
+                    "ON feesT.Customer = matT.Class\n" +
+                    "WHERE Disposition = 'Customer MRB' AND ncT.NCR_Date >= '" + startDateTimePicker.Value.ToShortDateString() + "' AND ncT.NCR_Date <= '" + endDateTimePicker.Value.ToShortDateString() + "';";
+            ExecuteQuery(query, 3, DataType.DoubleT, jBoss_connectionString);
 
             // calculate lost revenue cost
-            query = "SELECT 100*" + startDateTimePicker.Value.DayOfYear * endDateTimePicker.Value.DayOfYear;
-            ExecuteQuery(query, 5, DataType.DoubleT);
+            query = "SELECT SUM(Act_Scrap_Qty*soT.Unit_Price) AS Total\n" +
+                    "FROM [PRODUCTION].[dbo].[Job_Operation] AS scrapCountT\n" +
+                    "LEFT OUTER JOIN	PRODUCTION.dbo.SO_Detail AS soT\n" +
+                    "ON soT.Job = scrapCountT.Job\n" +
+                    "WHERE scrapCountT.Act_Scrap_Qty > 0 AND scrapCountT.Last_Updated >= '" + startDateTimePicker.Value.ToShortDateString() + "' AND scrapCountT.Last_Updated <= '" + endDateTimePicker.Value.ToShortDateString() + "';";
+            ExecuteQuery(query, 4, DataType.DoubleT, jBoss_connectionString);
 
             // calculate wasted capacity cost
-            query = "SELECT 100*" + startDateTimePicker.Value.DayOfYear * endDateTimePicker.Value.DayOfYear;
-            ExecuteQuery(query, 6, DataType.DoubleT);
+            query = "SELECT Sum((sumCountT.Act_Labor_Burden)/(sumCountT.Act_Run_Qty + sumCountT.Act_Scrap_Qty)*scrapCountT.Act_Scrap_Qty)\n" +
+                    "FROM [PRODUCTION].[dbo].[Job_Operation] AS scrapCountT\n" +
+                    "LEFT OUTER JOIN [PRODUCTION].[dbo].[Job_Operation] AS sumCountT\n" +
+                    "ON scrapCountT.[Job] = sumCountT.[Job] AND sumCountT.Sequence <= scrapCountT.Sequence\n" +
+                    "WHERE scrapCountT.Act_Scrap_Qty > 0 AND scrapCountT.Last_Updated >= '" + startDateTimePicker.Value.ToString() + "' AND sumCountT.Act_Run_Qty + sumCountT.Act_Scrap_Qty > 0 AND scrapCountT.Last_Updated <= '" + endDateTimePicker.Value.ToString() + "';";
+            ExecuteQuery(query, 5, DataType.DoubleT, jBoss_connectionString);
 
             // calculate recovery lot cost
-            query = "SELECT 100*" + startDateTimePicker.Value.DayOfYear * endDateTimePicker.Value.DayOfYear;
-            ExecuteQuery(query, 7, DataType.DoubleT);
+            query = "SELECT 0";
+            ExecuteQuery(query, 6, DataType.DoubleT, jBoss_connectionString);
 
             // calculate delivery cost
-            query = "SELECT 100*" + startDateTimePicker.Value.DayOfYear * endDateTimePicker.Value.DayOfYear;
-            ExecuteQuery(query, 8, DataType.DoubleT);
+            query = "SELECT 0";
+            ExecuteQuery(query, 7, DataType.DoubleT, jBoss_connectionString);
 
             // calcualte reputation cost
-            query = "SELECT 100*" + startDateTimePicker.Value.DayOfYear * endDateTimePicker.Value.DayOfYear;
-            ExecuteQuery(query, 9, DataType.DoubleT);
+            query = "SELECT 0";
+            ExecuteQuery(query, 8, DataType.DoubleT, jBoss_connectionString);*/
         }
 
-        private void ExecuteQuery(string query, int rowIndex, DataType type)
+        private void ExecuteQuery(string query, int rowIndex, DataType type, string connectionString)
         {
-            using(OdbcConnection conn = new OdbcConnection(jBoss_connectionString))
+            using (OdbcConnection conn = new OdbcConnection(connectionString))
             {
                 conn.Open();
 
+                // run query
                 OdbcCommand comm = new OdbcCommand(query, conn);
-
                 OdbcDataReader reader = comm.ExecuteReader();
 
-                DataGridViewCell currentCell = DetailsDataGridView.Rows[rowIndex].Cells[0];
+                // cell references
+                DataGridViewCell currentCellValue = DetailsDataGridView.Rows[rowIndex].Cells[0];
+                DataGridViewCell currentCellHours = DetailsDataGridView.Rows[rowIndex].Cells[1];
 
-                // read value
-                reader.Read();
+                // read row (check it exists)
+                if(reader.Read() == null)
+                    return;
 
-                switch(type)
+                // current column index
+                int col = 0;
+
+                switch (Type.GetTypeCode(reader.GetFieldType(col)))
                 {
-                    case DataType.DateT:
-                        currentCell.Value = reader.GetDateTime(0);
+                    case TypeCode.Decimal:
+                        currentCellValue.Value = reader.GetDecimal(col);
                         break;
-                    case DataType.DoubleT:
-                        currentCell.Value = reader.GetDouble(0);
+                    case TypeCode.Double:
+                        currentCellValue.Value = reader.GetDouble(col);
                         break;
-                    case DataType.StringT:
-                        currentCell.Value = reader.GetString(0);
+                    case TypeCode.DateTime:
+                        currentCellValue.Value = reader.GetDateTime(col);
+                        break;
+                    case TypeCode.String:
+                        currentCellValue.Value = reader.GetString(col);
+                        break;
+                    case TypeCode.Int32:
+                        currentCellValue.Value = reader.GetInt32(col);
                         break;
                 }
-            }
-        }
 
-        private void PopulateNCCPAData()
-        {
-            // first find the ncs from this year
-            using (OdbcConnection conn = new OdbcConnection(uni_connectionString))
-            {
-                // open connection to database
-                conn.Open();
+                // check if row has a second value (index 1)
+                if (reader.IsDBNull(1))
+                    return;
 
-                // query for nc data
-                string query = "SELECT NCR,NCR_Date, Material, Job, Reference, Qty_Scrap, Origin, Origin_ref, Origin_cause\n" +
-                                "FROM uniPoint_Live.dbo.PT_NC\n" +
-                                "WHERE NCR_Date >= " + startDateTimePicker.Value.ToShortDateString() + " AND NCR_Date <= " + endDateTimePicker.Value.ToShortDateString() + ";";
+                // increase column reference
+                col++;
 
-                OdbcCommand com = new OdbcCommand(query, conn);
-                OdbcDataReader reader = com.ExecuteReader();
-
-                while (reader.Read())
+                switch (Type.GetTypeCode(reader.GetFieldType(col)))
                 {
-
+                    case TypeCode.Decimal:
+                        currentCellHours.Value = reader.GetDecimal(col);
+                        break;
+                    case TypeCode.Double:
+                        currentCellHours.Value = reader.GetDouble(col);
+                        break;
+                    case TypeCode.DateTime:
+                        currentCellHours.Value = reader.GetDateTime(col);
+                        break;
+                    case TypeCode.String:
+                        currentCellHours.Value = reader.GetString(col);
+                        break;
+                    case TypeCode.Int32:
+                        currentCellHours.Value = reader.GetInt32(col);
+                        break;
                 }
+
             }
         }
     }
